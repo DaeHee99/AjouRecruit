5aeb9f07f8a78c4c673df54c1c29436521a11397
lacqto <128357523+lacqto@users.noreply.github.com>
2023-10-29 01:28:12 +0900

    feat: `landingpage` 용도변경

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,26 +1,21 @@
-import {useNavigate} from "react-router-dom";
-import {useState,useEffect} from "react";
-
-const image1 = "/img/resume.jpg"
-const image2 = "/img/replace.jpg"
+import mainVideo from "../../assets/Video/video.mp4"
 
 function LandingPage() { 
-  const navigation = useNavigate();
-  const [imageList] = useState([image1, image2, image1, image2]);
-  const [imageIdx, setImageIdx] = useState(0);
+  return (
+    <div className='absolute top-0 left-0 w-full h-full z-0'>
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className='w-full h-full object-cover'
+      >
+        <source src={mainVideo} type='video/mp4' />
+      </video>
+    </div>
 
-  useEffect(() => {
-    //  setInterval(() => {
-    //    setImageIdx(imageIdx=> (imageIdx+1) % 4);
-    //  }, 6000)
-  }, [])
-  
-  return <div className="flex-wrap m-6 ">
-    <div className="w-screen text-4xl text-center mb-5">인삿말 줄바꿈이 일어날떄까지 쓰는 말 </div>
-    <div className="h-96"><img id="image_id" src={imageList[imageIdx]} alt="no images" className="max-w-sm rounded-lg"/></div>
-    <div className="h-96"></div>
-    
-  </div>
+
+  );
 }  
   
 
